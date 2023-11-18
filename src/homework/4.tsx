0f6b7e9a618f03804201e8f9f56110b555bd20e2
pylypchukroman <romanpylypchuk@gmail.com>
2023-11-18 09:21:05 +0200

    added type for useState in task 3 and changed null to empty string in task 4

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -4,14 +4,14 @@ import noop from "lodash/noop";
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
-type SelectedMenu = { id: MenuIds | null };
+type SelectedMenu = { id: MenuIds | '' };
 type MenuSelected = { selectedMenu: SelectedMenu };
 type MenuAction = { onSelectedMenu: (menu: SelectedMenu) => void };
 
 // Додати тип Menu Selected
 
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: { id: null },
+  selectedMenu: { id: '' },
 });
 
 // Додайте тип MenuAction
@@ -26,7 +26,7 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити {id}
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({ id: null });
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({ id: '' });
 
   const menuContextAction = useMemo(
     () => ({
